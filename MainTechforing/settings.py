408c4ab33fee9f3585f1c43ff0b8e834accf0a6c
nuruzzaman99 <nuruzzamanmoyem.99@gmail.com>
2022-07-23 11:57:20 +0600

    solve

--- MainTechforing/settings.py
+++ MainTechforing/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = 'django-insecure-j55@su5sxajtqf_yed#+^vn&p0l=ovow1^6cp$&05so^8^l3th
 DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0', '44.242.38.198', 'main.techforing.com', 'pcs.techforing.com', 'training.techforing.com',
-                 '127.0.0.1', 'localhost', '.techforing.com','52.10.227.27','root.techforing.com','academy.techforing.com','school.techfoirng.com']
+                 '127.0.0.1', 'localhost', 'blog.techforing.com','52.10.227.27','root.techforing.com','academy.techforing.com','school.techfoirng.com']
 
 # Application definition
 
